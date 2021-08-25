dfec41519cf5af0c04e98389fce1e09deaa449dd
Jason-Peralta <jperalta3197@gmail.com>
2021-08-25 17:32:34 -0400

    Added api implementation

--- src/components/apibutton.js
+++ src/components/apibutton.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button} from "evergreen-ui";
 
 export default function ApiButton(props) {
-    return (
-        <Button onClick={()=>{console.log(props.answers)}}/>
-    )
+    const [Weather, setWeather] = React.useState(null)
 
+    const fetchWeather = async () => {
+        setWeather(
+            await fetch("http://localhost:3000/weather/KPHX/?actual_max_temp[eq]=104&actual_mean_temp[gt]=94")
+                .then(response => response.json())
+                .then(json => json.KPHX)
+        )
+    }
+
+    useEffect(() => {
+        document.title = "Weather Application";
+        fetchWeather().then(x => x)
+    }, []);
+
+    if (Weather === ""){
+        return(
+            <div>
+                Data Not Loaded.
+            </div>
+        )
+    } else{
+
+    return (
+
+        <Button onClick={()=>{fetchWeather()}}> {Weather} </Button>
+    )
+    }
 }
